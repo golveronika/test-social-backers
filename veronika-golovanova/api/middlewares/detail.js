def9cd60b69be9d54b04709faefb2f92c66b4d23
Вероника Голованова <veronika@MacBook-Pro-Veronika-2.local>
2020-06-18 16:30:29 +0200

    add 2 endpoints B3

--- veronika-golovanova/api/middlewares/detail.js
+++ veronika-golovanova/api/middlewares/detail.js
@@ -1,1 +1,21 @@
-module.exports = function(req, res, next) {}
+const fetch = require('node-fetch')
+
+module.exports = async function(req, res, next) {
+
+	try {
+		const response = await fetch(`https://api.spacexdata.com/v3/launches?flight_number=${req.params.id}`)
+		if (!response.ok) throw new Error(response.statusCode)
+		const launches = await response.json()
+
+		const output = launches.map(
+			({ flight_number, mission_name, launch_date_utc }) => ({
+				flight_number,
+				mission_name,
+				launch_date: launch_date_utc
+			})
+		)
+		res.json(output)
+	} catch (e) {
+		next(e)
+	}
+}
--- veronika-golovanova/api/middlewares/ship-stats.js
+++ veronika-golovanova/api/middlewares/ship-stats.js
@@ -1,1 +1,20 @@
-module.exports = function(req, res, next) {}
+const fetch = require('node-fetch')
+
+module.exports = async function(req, res, next) {
+
+	try {
+		const response = await fetch(`https://api.spacexdata.com/v3/ships?ship_id=${req.params.ids}`)
+		if (!response.ok) throw new Error(response.statusCode)
+		const launches = await response.json()
+
+		const output = launches.map(
+			({ weight_kg, ship_name }) => ({
+				total_weight: weight_kg,
+				oldest_ship: ship_name,
+			})
+		)
+		res.json(output)
+	} catch (e) {
+		next(e)
+	}
+}
