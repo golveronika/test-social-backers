dd0aaf9846958484945bc8ee4cbefabab01e133a
Вероника Голованова <veronika@MacBook-Pro-Veronika-2.local>
2020-06-18 16:00:33 +0200

    fix B1) Fix bug in /launches/all

--- veronika-golovanova/api/middlewares/all.js
+++ veronika-golovanova/api/middlewares/all.js
@@ -5,13 +5,16 @@ module.exports = async function(req, res, next) {
 		const response = await fetch('https://api.spacexdata.com/v3/launches')
 		if (!response.ok) throw new Error(response.statusCode)
 		const launches = await response.json()
+
 		const output = launches.map(
-			({ flight_number, mission_name, launch_date }) => ({
+			({ flight_number, mission_name, launch_date_utc }) => ({
 				flight_number,
 				mission_name,
-				launch_date,
+				launch_date: launch_date_utc
 			})
 		)
+
+		console.log("output", output);
 
 		res.json(output)
 	} catch (e) {
